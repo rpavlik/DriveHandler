b7d30e71362e9300d33405dbe1eb125de9765d7b
Ryan Pavlik <rpavlik@iastate.edu>
2011-05-04 14:13:14 -0500

    add license and headers

--- DriveHandler/ConfigFileHandler.py
+++ DriveHandler/ConfigFileHandler.py
@@ -1,3 +1,13 @@
+# Part of DriveHandler
+# Author: Ryan Pavlik
+
+# https://github.com/rpavlik/DriveHandler
+
+#          Copyright Iowa State University 2011.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
 import os
 import shutil
 
--- sillyexample.py
+++ sillyexample.py
@@ -1,5 +1,14 @@
-# requires packages:
-# python python-gobject python-dbus dbus udisks
+#!/usr/bin/env python
+# Part of DriveHandler
+# Author: Ryan Pavlik
+
+# https://github.com/rpavlik/DriveHandler
+
+#          Copyright Iowa State University 2011.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
 import gobject
 from dbus.mainloop.glib import DBusGMainLoop
 import subprocess
