5f6e71d2da42b6779b7619827e859d72dbb32a94
Ryan Pavlik <rpavlik@iastate.edu>
2011-05-04 13:37:47 -0500

    add exception callbacks and postunmount callbacks

--- DriveHandler/DeviceListener.py
+++ DriveHandler/DeviceListener.py
@@ -1,15 +1,14 @@
 import dbus
 
 class DeviceListener(object):
-  def __init__(self, handlerTypes = []):
+  def __init__(self, handlerTypes = [], postUnmountCallbacks = [], exceptionCallbacks = []):
     self.bus = dbus.SystemBus()
     self.ud_manager_obj = self.bus.get_object("org.freedesktop.UDisks", "/org/freedesktop/UDisks")
     self.ud_manager = dbus.Interface(self.ud_manager_obj, 'org.freedesktop.UDisks')
     self.ud_manager.connect_to_signal("DeviceAdded", self.handleDevice)
     self.handlerTypes = handlerTypes
-
-  def __beep(self):
-    print "\a"
+    self.postUnmountCallbacks = postUnmountCallbacks
+    self.exceptionCallbacks = exceptionCallbacks
 
   def handleDevice(self, dev):
     drive = USBDrive(dev)
@@ -20,10 +19,11 @@ class DeviceListener(object):
         h.run()
     except Exception as ex:
       print "Caught an exception... %s" % ex
-
-      beep()
+      for callback in self.exceptionCallbacks:
+        callback(ex)
     finally:
       drive.unmount()
       drive.eject()
-      beep()
+      for callback in self.postUnmountCallbacks:
+        callback()
 
---
+++ usingudisks.py
@@ -0,0 +1,29 @@
+# requires packages:
+# python python-gobject python-dbus dbus udisks
+import gobject
+from dbus.mainloop.glib import DBusGMainLoop
+import subprocess
+
+import DriveHandler
+
+class NetworkConfigHandler(DriveHandler.ConfigFileHandler):
+  def __init__(self, drive):
+    super(NetworkConfigHandler, self).__init__(drive, "/etc/network/interfaces")
+
+  def postUpdate(self):
+    print "Restarting networking..."
+    subprocess.call(["service", "network-manager", "restart"])
+
+def beep():
+  print "\a"
+# Must come before creating the listener
+DBusGMainLoop(set_as_default=True)
+
+listener = DriveHandler.DeviceListener(
+  handlerTypes = [NetworkConfigHandler],
+  postUnmountCallbacks = [beep],
+  exceptionCallbacks = [beep]
+)
+
+loop = gobject.MainLoop()
+loop.run()
